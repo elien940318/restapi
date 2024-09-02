a3f51c4fc4703f7de2ff6abdd0f1b0f642253e28
changgi Kang <elien940318@gmail.com>
2024-09-02 23:01:18 +0900

    API Versioning

---
+++ src/main/java/com/changkeereum/restapi/bean/AdminUserV2.java
@@ -0,0 +1,17 @@
+package com.changkeereum.restapi.bean;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonFilter("UserInfoV2")
+public class AdminUserV2 extends AdminUser {
+
+    private String grade;
+
+}
--- src/main/java/com/changkeereum/restapi/controller/AdminUserController.java
+++ src/main/java/com/changkeereum/restapi/controller/AdminUserController.java
@@ -1,5 +1,8 @@
 package com.changkeereum.restapi.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.changkeereum.restapi.bean.AdminUser;
+import com.changkeereum.restapi.bean.AdminUserV2;
 import com.changkeereum.restapi.bean.User;
 import com.changkeereum.restapi.dao.UserDaoService;
 import com.changkeereum.restapi.exception.UserNotFoundException;
@@ -31,7 +35,10 @@ public class AdminUserController {
      * @param id
      * @return
      */
-    @GetMapping("/users/{id}")
+    // @GetMapping("/v1/users/{id}")
+    // @GetMapping(value="/users/{id}", params="version=1")
+    // @GetMapping(value="/users/{id}", headers="X-API-VERSION=1")
+    @GetMapping(value="/users/{id}", produces="application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUserForAdmin(@PathVariable("id") int id) {
         User user = service.findOne(id);
 
@@ -46,6 +53,51 @@ public class AdminUserController {
         SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
         
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    // @GetMapping("/v2/users/{id}") // URL Versioning; Twitter
+    // @GetMapping(value="/users/{id}", params="version=2") // Request Parameter Versioning; Amazon
+    // @GetMapping(value="/users/{id}", headers="X-API-VERSION=2") // (Custom) Headers Versioning; Microsoft
+    @GetMapping(value="/users/{id}", produces="application/vnd.company.appv2+json") // MIME Type Versioning; GitHub
+    public MappingJacksonValue retrieveUserForAdminV2(@PathVariable("id") int id) {
+        User user = service.findOne(id);
+
+        AdminUserV2 adminUser = new AdminUserV2();
+
+        if(ObjectUtils.isEmpty(user)) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        BeanUtils.copyProperties(user, adminUser);
+        adminUser.setGrade("VIP");
+        
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "grade");
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+        
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+    
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsersForAdmin() {
+        List<User> users = service.findAll();
+
+        List<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+        for(User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+            adminUsers.add(adminUser);
+        }
+        
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+        
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
         mapping.setFilters(filters);
 
         return mapping;
